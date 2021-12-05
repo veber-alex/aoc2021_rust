4140fae5bcc769b8082796cd3438071951cdef13
Alex Veber <alexveber@gmail.com>
2021-12-05 04:20:46 +0200

    Compile on stable

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -1,5 +1,3 @@
-#![feature(array_windows)]
-
 use std::fs;
 
 use anyhow::Result;
@@ -9,14 +7,14 @@ fn parse_input(input: &str) -> Result<Vec<usize>> {
 }
 
 fn num_of_increments(depths: &[usize]) -> usize {
-    depths.array_windows().filter(|[a, b]| b > a).count()
+    depths.windows(2).filter(|w| w[1] > w[0]).count()
 }
 
 fn num_of_increments_3(depths: &[usize]) -> usize {
     let mut a = usize::MAX;
     depths
-        .array_windows()
-        .map(|[a, b, c]| a + b + c)
+        .windows(3)
+        .map(|w| w[0] + w[1] + w[2])
         .filter(|&b| {
             let inc = b > a;
             a = b;
