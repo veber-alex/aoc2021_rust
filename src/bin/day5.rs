5cf65a6e559ed3363965142034ebd9d984ebe95a
Alex Veber <alexveber@gmail.com>
2021-12-05 18:37:00 +0200

    Seperate logic in Day 5

--- src/bin/day5.rs
+++ src/bin/day5.rs
@@ -5,69 +5,70 @@ fn parse_point(s: &str) -> (u32, u32) {
     (a.parse().unwrap(), b.parse().unwrap())
 }
 
-fn day5a(input: &str) -> usize {
-    use std::cmp::Ordering::*;
-
-    let mut points: HashMap<(u32, u32), usize> = HashMap::new();
-    let mut add_point = |x, y| *points.entry((x, y)).or_default() += 1;
+fn parse_and_run<F>(input: &str, mut f: F) -> usize
+where
+    F: FnMut(u32, u32, u32, u32, &mut HashMap<(u32, u32), bool>),
+{
+    let mut points = HashMap::new();
 
     for line in input.lines() {
         let (start, end) = line.split_once(" -> ").unwrap();
         let (xs, ys) = parse_point(start);
         let (xe, ye) = parse_point(end);
 
-        match (xs.cmp(&xe), ys.cmp(&ye)) {
-            (Less, Equal) => (xs..=xe).for_each(|x| add_point(x, ys)),
-            (Greater, Equal) => (xe..=xs).for_each(|x| add_point(x, ys)),
-            (Equal, Less) => (ys..=ye).for_each(|y| add_point(xs, y)),
-            (Equal, Greater) => (ye..=ys).for_each(|y| add_point(xs, y)),
-            _ => continue,
-        };
+        f(xs, ys, xe, ye, &mut points);
     }
 
-    points.values().filter(|v| **v > 1).count()
+    points.values().filter(|v| **v).count()
 }
 
-fn day5b(input: &str) -> usize {
+fn day5a(xs: u32, ys: u32, xe: u32, ye: u32, points: &mut HashMap<(u32, u32), bool>) {
     use std::cmp::Ordering::*;
 
-    let mut points: HashMap<(u32, u32), usize> = HashMap::new();
-    let mut add_point = |x, y| *points.entry((x, y)).or_default() += 1;
-
-    for line in input.lines() {
-        let (start, end) = line.split_once(" -> ").unwrap();
-        let (xs, ys) = parse_point(start);
-        let (xe, ye) = parse_point(end);
-
-        match (xs.cmp(&xe), ys.cmp(&ye)) {
-            (Less, Equal) => (xs..=xe).for_each(|x| add_point(x, ys)),
-            (Greater, Equal) => (xe..=xs).for_each(|x| add_point(x, ys)),
-            (Equal, Less) => (ys..=ye).for_each(|y| add_point(xs, y)),
-            (Equal, Greater) => (ye..=ys).for_each(|y| add_point(xs, y)),
-            (Less, Less) => (xs..=xe).zip(ys..=ye).for_each(|(x, y)| add_point(x, y)),
-            (Less, Greater) => (xs..=xe)
-                .zip((ye..=ys).rev())
-                .for_each(|(x, y)| add_point(x, y)),
-            (Greater, Less) => (xe..=xs)
-                .rev()
-                .zip(ys..=ye)
-                .for_each(|(x, y)| add_point(x, y)),
-            (Greater, Greater) => (xe..=xs).zip(ye..=ys).for_each(|(x, y)| add_point(x, y)),
-            (Equal, Equal) => unreachable!(),
-        };
+    let mut add_point = |x, y| {
+        points.entry((x, y)).and_modify(|v| *v = true).or_default();
+    };
+    match (xs.cmp(&xe), ys.cmp(&ye)) {
+        (Less, Equal) => (xs..=xe).for_each(|x| add_point(x, ys)),
+        (Greater, Equal) => (xe..=xs).for_each(|x| add_point(x, ys)),
+        (Equal, Less) => (ys..=ye).for_each(|y| add_point(xs, y)),
+        (Equal, Greater) => (ye..=ys).for_each(|y| add_point(xs, y)),
+        _ => {}
     }
+}
 
-    points.values().filter(|v| **v > 1).count()
+fn day5b(xs: u32, ys: u32, xe: u32, ye: u32, points: &mut HashMap<(u32, u32), bool>) {
+    use std::cmp::Ordering::*;
+
+    let mut add_point = |x, y| {
+        points.entry((x, y)).and_modify(|v| *v = true).or_default();
+    };
+    match (xs.cmp(&xe), ys.cmp(&ye)) {
+        (Less, Equal) => (xs..=xe).for_each(|x| add_point(x, ys)),
+        (Greater, Equal) => (xe..=xs).for_each(|x| add_point(x, ys)),
+        (Equal, Less) => (ys..=ye).for_each(|y| add_point(xs, y)),
+        (Equal, Greater) => (ye..=ys).for_each(|y| add_point(xs, y)),
+        (Less, Less) => (xs..=xe).zip(ys..=ye).for_each(|(x, y)| add_point(x, y)),
+        (Less, Greater) => (xs..=xe)
+            .zip((ye..=ys).rev())
+            .for_each(|(x, y)| add_point(x, y)),
+        (Greater, Less) => (xe..=xs)
+            .rev()
+            .zip(ys..=ye)
+            .for_each(|(x, y)| add_point(x, y)),
+        (Greater, Greater) => (xe..=xs).zip(ye..=ys).for_each(|(x, y)| add_point(x, y)),
+        (Equal, Equal) => unreachable!(),
+    };
 }
 
 fn main() {
     let input = include_str!("../../inputs/day5.txt");
 
-    let day5a = day5a(input);
+    let day5a = parse_and_run(input, day5a);
     debug_assert_eq!(day5a, 5608);
     println!("day5a: {}", day5a);
 
-    let day5b = day5b(input);
+    let day5b = parse_and_run(input, day5b);
     debug_assert_eq!(day5b, 20299);
     println!("day5b: {}", day5b);
 }
