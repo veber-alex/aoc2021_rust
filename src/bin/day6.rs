6065ed60dab8be603e3f4f20395c302b6c5cc465
Alex Veber <alexveber@gmail.com>
2021-12-06 19:09:34 +0200

    Day 6

---
+++ src/bin/day6.rs
@@ -0,0 +1,32 @@
+use std::ops::Range;
+
+fn parse_input(input: &str) -> [usize; 9] {
+    let mut arr = [0; 9];
+
+    for s in input.split(',') {
+        arr[s.parse::<usize>().unwrap()] += 1;
+    }
+
+    arr
+}
+
+fn day6(arr: &mut [usize; 9], gen_range: Range<usize>) -> usize {
+    for g in gen_range {
+        arr[(g + 7) % 9] += arr[g % 9];
+    }
+
+    arr.iter().sum()
+}
+
+fn main() {
+    let input = include_str!("../../inputs/day6.txt");
+    let mut arr = parse_input(input);
+
+    let day6a = day6(&mut arr, 0..80);
+    debug_assert_eq!(day6a, 359344);
+    println!("day6a: {}", day6a);
+
+    let day6b = day6(&mut arr, 80..256);
+    debug_assert_eq!(day6b, 1629570219571);
+    println!("day6b: {}", day6b);
+}
