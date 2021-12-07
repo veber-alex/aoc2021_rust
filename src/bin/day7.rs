929b5c1ba0ceb0be1b4adeb9aeb53c0e71d1f703
Alex Veber <alexveber@gmail.com>
2021-12-07 11:59:25 +0200

    Day 7

---
+++ src/bin/day7.rs
@@ -0,0 +1,44 @@
+fn parse_input(input: &str) -> Vec<u32> {
+    input.split(',').map(|s| s.parse().unwrap()).collect()
+}
+
+fn day7a(data: &[u32]) -> u32 {
+    data.iter()
+        .copied()
+        .map(|pos| data.iter().copied().map(|v| pos.max(v) - pos.min(v)).sum())
+        .min()
+        .unwrap()
+}
+
+fn day7b(data: &[u32]) -> u32 {
+    (1..)
+        .scan(u32::MAX, |best, pos| {
+            let d = data
+                .iter()
+                .copied()
+                .map(|v| {
+                    let n = pos.max(v) - pos.min(v);
+                    n * (n + 1) / 2
+                })
+                .sum();
+            (d < *best).then(|| {
+                *best = d;
+                d
+            })
+        })
+        .last()
+        .unwrap()
+}
+
+fn main() {
+    let input = include_str!("../../inputs/day7.txt");
+    let data = parse_input(&input);
+
+    let day7a = day7a(&data);
+    debug_assert_eq!(day7a, 343441);
+    println!("day7a: {}", day7a);
+
+    let day7b = day7b(&data);
+    debug_assert_eq!(day7b, 98925151);
+    println!("day7b: {}", day7b);
+}
